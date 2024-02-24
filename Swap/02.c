92bdaa9f484d409642f6e762176557696f251ce4
Mohd Mohsin Khan <mohdmohsinkhanee@gmail.com>
2024-02-25 01:30:02 +0530

    Swapping two variables without using Third Variable and in One shot Method-1 and 2

---
+++ Swap/02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*WAP to swap two numbers without using Third variable and in one shot*/
+int main()
+{
+        int a=10;
+        int b=20;
+        printf("a-> %d and b-> %d\n",a, b); 
+        a = a * b / (b=a);
+        printf("After swapping\na-> %d and b-> %d\n",a, b); 
+        return 0;
+}
